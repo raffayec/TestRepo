329bb3f23ef5e410ca6d52191670999a4528fe41
Elena Raffay <elena.c.raffay@vanderbilt.edu>
2016-05-20 15:20:46 -0400

    First Files

---
+++ C and E Test/C and E Test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="C_and_E_Test.MvcApplication" Language="C#" %>
---
+++ C and E Test/C and E Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(C_and_E_Test.Startup))]
+namespace C_and_E_Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
